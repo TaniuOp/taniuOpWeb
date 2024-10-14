2675d507c8c52921a43f42be48b057489eca1c79
TaniuBM <taniuop@gmail.com>
2024-10-14 19:30:21 +0200

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import MainContainer from './components/PortfolioSections/MainContainer';
 import Footer from './components/PortfolioSections/Footer';
 import { BrowserRouter } from 'react-router-dom';
+import origamiBackground from './assets/wild_animals.jpg';
 import './styles/styles.scss';
 
 function App() {
   return (
     <>
-      <div className='App'>
+      <div
+        className='App'
+        style={{
+          backgroundImage: `url(${origamiBackground})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          minHeight: '100vh',
+        }}
+      >
         <BrowserRouter>
           <MainContainer />
         </BrowserRouter>
