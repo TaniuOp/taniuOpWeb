316b796c4d22b1c10026c695fcc8da19e83b9416
TaniuBM <taniuop@gmail.com>
2024-11-05 09:38:44 +0100

    update file to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // import MainContainer from './components/PortfolioSections/MainContainer';
 import Footer from './components/PortfolioSections/Footer';
-import { BrowserRouter } from 'react-router-dom';
 import './styles/styles.scss';
 import backgroundImg from './assets/abstract_compose.png';
 import Landing from './components/Wip/Landing';
@@ -14,10 +13,8 @@ function App() {
           alt='taniu_background'
           className='img-abstract'
         />
-        <BrowserRouter>
-          {/* <MainContainer /> */}
-          <Landing />
-        </BrowserRouter>
+        {/* <MainContainer /> */}
+        <Landing />
       </div>
       <Footer />
     </>
